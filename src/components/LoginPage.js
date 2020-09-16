c17d6ceff70198efd8807bd8e115ddc577860cb6
Albert2645 <batuhannbirkan@gmail.com>
2020-09-16 21:56:00 +0300

    base

---
+++ src/components/LoginPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './LoginPage.scss';
+
+const LoginPage = () => {
+    return (
+        <div className="bg">
+            <div className="container">
+                <div className="login-panel">
+                    <form action="#">
+                        <div className="input-elements">
+                            <input type="text" className="username" placeholder="Username" />
+                            <input type="text" className="passwd" placeholder="Password" />
+                            <input type="submit" className="sendit" value="Giris" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default LoginPage;
