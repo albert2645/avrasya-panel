7130e801b40c226142425e02081e239b4c22a0d1
Efe <jimmyfallon717@gmail.com>
2020-09-17 02:21:56 +0300

    Responsive fix. header added.

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -8,6 +8,7 @@ const LoginPage = () => {
                 <div className="login-panel">
                     <form action="#">
                         <div className="input-elements">
+                        <label className="login-header">Login</label>
                             <input type="text" className="username" placeholder="Username" />
                             <input type="text" className="passwd" placeholder="Password" />
                             <input type="submit" className="sendit" value="Giris" />
